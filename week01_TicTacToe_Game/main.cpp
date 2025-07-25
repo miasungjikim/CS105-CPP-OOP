e6c63d2ba9a7efc50935161d674d6a70fa27cc95
sungjikim <miasungji@gmail.com>
2025-07-25 21:31:05 +1200

    Add board initialization and display function

--- week01_TicTacToe_Game/main.cpp
+++ week01_TicTacToe_Game/main.cpp
@@ -2,12 +2,58 @@
 // Created by MiaKim on 25/07/2025.
 //
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
+class TicTacToe{
+private:
+    vector<vector<char>> gameboard;
+    char currentPlayer = 'X';
 
-    cout << "Hello World";
+public:
+    //Gameboard (3x3)
+    void resetBoard() {
+        gameboard.clear(); //reset gameboard. otherwise, board getting bigger
+        char symbolNumber = '1'; // why char ? cuz vector<vector<char>> gameboard
+
+        for (int i =0; i < 3; i ++) {
+            vector<char>row;
+            for (int j =0; j <3; j++) {
+                row.push_back(symbolNumber); //add row
+                symbolNumber ++;
+            }
+            gameboard.push_back(row);
+        }
+    }
+
+    void displayBoard() {
+        for (int i =0; i <3; i++) {
+            for (int j = 0; j < 3; j++) {
+                cout << gameboard[i][j];
+               if (j < 2) cout << " | ";
+            }
+            cout << endl;
+            if (i < 2) cout << "--+---+---" << endl;
+        }
+    }
+
+
+
+    void play() {
+        resetBoard();
+        displayBoard();
+    }
+
+
+
+};
+
+int main() {
+    TicTacToe game;
+
+    game.play();
+
 
     return 0;
 }
